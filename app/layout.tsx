03c35cbd94f2520c6adb7b272f6b511e50659a37
sianmatti <sianmatti11@gmail.com>
2026-06-15 06:25:38 +0800

    Fix production GTM execution

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import { sitePath } from "@/lib/site";
 import "./globals.css";
 
@@ -59,13 +58,15 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Sora:wght@400;500;600&display=swap"
           rel="stylesheet"
         />
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');`}
-        </Script>
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');`,
+          }}
+        />
       </head>
       <body>
         <noscript>
